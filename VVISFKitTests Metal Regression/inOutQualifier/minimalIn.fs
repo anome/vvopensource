0b4ec68c1c830f4c605d1d56e0f2a66daa65fdbd
MTO <mto@anomes.com>
2024-03-11 16:55:39 +0100

    New : test shader files

---
+++ VVISFKitTests Metal Regression/inOutQualifier/minimalIn.fs
@@ -0,0 +1,15 @@
+/*{
+    "DESCRIPTION": "Basic shader using in keyword",
+    "CREDIT": "MTO",
+}*/
+
+    
+vec4 turnGreen(in vec3 _)
+{
+    return vec4(0,100,0,1);
+}
+
+void main(void) {
+    vec3 a = vec3(1,1,1);
+    gl_FragColor = turnGreen(a);
+}
